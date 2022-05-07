9d76f66220d263454a623c2720dbcb974ba4f335
rejRoky <rejaul.islam.roky@gmail.com>
2022-05-07 22:41:46 +0600

    updated

---
+++ Recursion/StaticGlobalVariable/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+void fun (int n)
+{
+    if (n > 40)
+    {
+        cout <<  n << endl;
+        fun(n-1); // recursion perform
+
+    }
+}
+
+// Exp :
+// 1. Switch on bulb
+// 2. goto next Room
+
+void room (int numberOfRoom) // recursion funtion
+{
+    if (numberOfRoom > 0)
+    {
+        cout << "Switch Bulb of Romm No : " << numberOfRoom << endl; // Switch on bulb
+        room(numberOfRoom-1); // goto next room
+        cout << "Come Back in room no: " << numberOfRoom << endl;
+
+    }
+}
+
+int a = 10;
+
+
+int main()
+{
+    fun(a); // funtion call
+    room(a);
+    return 0;
+}
